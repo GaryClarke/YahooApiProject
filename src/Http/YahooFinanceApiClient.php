bca3a3f509fe0f49511c6168c294ea853e680cf2
Gary Clarke <gary@pack-net.co.uk>
2020-12-28 16:08:45 +0000

    Create response content in YFAC. Update .gitignore

--- src/Http/YahooFinanceApiClient.php
+++ src/Http/YahooFinanceApiClient.php
@@ -25,17 +25,39 @@ class YahooFinanceApiClient
     public function fetchStockProfile($symbol, $region): array
     {
         $response = $this->httpClient->request('GET', self::URL, [
-            'query' => [
+            'query'   => [
                 'symbol' => $symbol,
                 'region' => $region
             ],
             'headers' => [
                 'x-rapidapi-host' => self::X_RAPID_API_HOST,
-                'x-rapidapi-key' => $this->rapidApiKey
+                'x-rapidapi-key'  => $this->rapidApiKey
             ]
         ]);
 
-        
+        // @todo handle non 200 responses
+        if ($response->getStatusCode() !== 200) {
+
+            // return a non 200 response here
+        }
+
+        $stockProfile = json_decode($response->getContent())->price;
+
+        $stockProfileAsArray = [
+            'symbol'        => $stockProfile->symbol,
+            'shortName'     => $stockProfile->shortName,
+            'region'        => $region,
+            'exchangeName'  => $stockProfile->exchangeName,
+            'currency'      => $stockProfile->currency,
+            'price'         => $stockProfile->regularMarketPrice->raw,
+            'previousClose' => $stockProfile->regularMarketPreviousClose->raw,
+            'priceChange'   => $stockProfile->regularMarketPrice->raw - $stockProfile->regularMarketPreviousClose->raw
+        ];
+
+        return [
+            'statusCode' => 200,
+            'content'    => json_encode($stockProfileAsArray)
+        ];
     }
 }
 
